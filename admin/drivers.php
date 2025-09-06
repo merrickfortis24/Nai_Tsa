e4c600a668d713115dd0e3a2a30dfa0bdc6cee14
merrickfortis24 <fortismerrick@gmail.com>
2025-09-06 13:52:21 +0800

    Fix database class path in drivers.php to ensure correct database connection

--- admin/drivers.php
+++ admin/drivers.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin_id'])) { header('Location: login.php'); exit; }
-require_once '../users/classes/database.php';
+require_once '../admin/classes/database.php';
 $db = new database();
 $con = $db->opencon();
 
